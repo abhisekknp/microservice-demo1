8f3050bb8f3d5c900a3bee951d22c5a6e9a9e32b
Abhishek.Omar <Abhishek.Omar@Kronos.com>
2020-08-23 14:03:18 +0530

    Committing below projects: Auth Server, Api Gateway, Zipkin Server, Eureca Server

---
+++ docker-image-all-projects.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd api-gateway; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build buildDocker; cd ..
+cd auth-server; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build buildDocker; cd ..
+cd config-server; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build buildDocker; cd ..
+cd webservice-registry; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build buildDocker; cd ..
+cd zipkin-server; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build buildDocker; cd ..
