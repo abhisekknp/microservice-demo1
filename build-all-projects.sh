3ddd8ae0b1541105d6cee7f78de22653c01cabda
Abhishek.Omar <Abhishek.Omar@Kronos.com>
2020-09-02 11:46:56 +0530

    movie-service changes

--- build-all-projects.sh
+++ build-all-projects.sh
@@ -4,4 +4,5 @@ cd api-gateway; echo "\033[1;96m Execution directory: `pwd | xargs basename` \03
 cd auth-server; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build; cd ..
 cd config-server; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build; cd ..
 cd webservice-registry; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build; cd ..
+cd movie-service; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build ; cd ..
 cd zipkin-server; echo "\033[1;96m Execution directory: `pwd | xargs basename` \033[0m"; ./gradlew clean build ; cd ..
